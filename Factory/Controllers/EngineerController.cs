d179ed4f7ea1948788718e40ebf7f0f548b4445a
Kevin Funez <kevin.funez315@gmail.com>
2021-10-15 13:20:59 -0700

    add Engineer controller

--- Factory/Controllers/EngineerController.cs
+++ Factory/Controllers/EngineerController.cs
@@ -15,10 +15,67 @@ namespace Factory.Controllers
       _db = db;
     }
 
+    [HttpGet("/engineers")]
     public ActionResult Index()
     {
       List<Engineer> model = _db.Engineers.ToList();
       return View(model);
     }
+
+    [HttpGet("/engineers/create")]
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost("/engineers/create")]
+    public ActionResult Create(Engineer engineer)
+    {
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/engineers/edit")]
+    public ActionResult Edit(int id)
+    {
+      var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+
+    [HttpPost("/engineers/edit")]
+    public ActionResult Edit(Engineer engineer)
+    {
+      _db.Entry(engineer).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/engineers/delete")]
+    public ActionResult Delete(int id)
+    {
+      var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    } 
+
+    [HttpPost("/engineers/delete"), ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      _db.Engineers.Remove(thisEngineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }  
+
+    [HttpGet("/engineers/details")]
+    public ActionResult Details(int id)
+    {
+      var thisEngineer = _db.Engineers
+          .Include(category => category.JoinEntities)
+          .ThenInclude(join => join.Machine)
+          .FirstOrDefault(category => category.EngineerId == id);
+      return View(thisEngineer);
+    }
+    
   }
 }
